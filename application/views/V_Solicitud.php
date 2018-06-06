fbd26f886c7b9b79eb4787825e25d288263ff936
joseminaya15 <jose.minayac15@gmail.com>
2018-06-06 16:35:27 -0500

    menu verticla

--- application/views/V_Solicitud.php
+++ application/views/V_Solicitud.php
@@ -36,7 +36,7 @@
     </div>
     <div class="mdl-layout mdl-js-layout mdl-layout--fixed-drawer">
         <div class="mdl-layout__drawer">
-            <span class="mdl-layout-title">Title</span>
+            <span class="mdl-layout-title">¿Qu&eacute; deseas hacer</span>
             <nav class="mdl-navigation">
                 <a class="mdl-navigation__link" href="">Ingresar una nueva cotizaci&oacute;n</a>
                 <a class="mdl-navigation__link" href="">Ver puntaje acumulado</a>
