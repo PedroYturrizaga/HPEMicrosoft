4d5ca8a350edc908e6902be744c16bf3d5ea66d1
joseminaya15 <jose.minayac15@gmail.com>
2018-06-08 10:45:20 -0500

    css js menu

--- public/js/solicitud.js
+++ public/js/solicitud.js
@@ -257,7 +257,6 @@ function goToMenu(id){
 	var idLink    = $('#'+id);
 	var idSection = $('#section-'+id)
 	$('.mdl-navigation__link').removeClass('active');
-	// idSection.addClass('class_name')
 	$('.js-section--menu').addClass('animated fadeOut');
 	idSection.removeClass('animated fadeOut');
 	idSection.addClass('animated fadeIn');
