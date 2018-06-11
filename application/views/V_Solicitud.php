7ea571b1d56929397f65fc29dde2161a9a9a6fea
joseminaya15 <jose.minayac15@gmail.com>
2018-06-11 17:58:40 -0500

    asda

--- application/views/V_Solicitud.php
+++ application/views/V_Solicitud.php
@@ -112,7 +112,7 @@
                                 <input type="text" id="monto" onchange="validarCampos()">
                             </div>
                             <div class="col-xs-12 js-input js-file js-flex">
-                                <input type="text" id="archivoDocumento" placeholder="Seleccione una imagen o pdf" name="archivoDocumento" disabled>
+                                <input type="text" id="archivoDocumento" placeholder="Suba su factura en imagen o pdf" name="archivoDocumento" disabled>
                                 <div class="js-icon">
                                     <button type="button" class="mdl-button mdl-js-button mdl-button--icon" onclick="subirFactura()">
                                         <i class="mdi mdi-backup"></i>
